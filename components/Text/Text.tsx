39cb0b1b6f4530ad14b01b8e64510c4945d4af79
secona <77039267+secona@users.noreply.github.com>
2023-06-24 09:18:47 +0700

    refactor: DRY code in Text.tsx

--- components/Text/Text.tsx
+++ components/Text/Text.tsx
@@ -3,48 +3,18 @@ import { GlobalStyles } from "@/styles/global";
 import { cnProps } from "@/utils/classnames";
 import styles from "./Text.module.scss";
 
+function handleProps({ fontSize, ...otherProps }: TextProps<any>) {
+  return cnProps(otherProps, styles[`text-fs-${fontSize}`]);
+}
+
 export type TextProps<T extends React.ElementType<any>> = {
   fontSize?: keyof GlobalStyles["fontSize"];
 } & React.ComponentPropsWithoutRef<T>;
 
-export const Text = ({ fontSize, ...props }: TextProps<"p">) => {
-  return (
-    <p {...cnProps(props, fontSize ? styles[`text-fs-${fontSize}`] : "")}></p>
-  );
-};
-
-Text.H1 = ({ fontSize, ...props }: TextProps<"h1">) => {
-  return (
-    <h1 {...cnProps(props, fontSize ? styles[`text-fs-${fontSize}`] : "")}></h1>
-  );
-};
-
-Text.H2 = ({ fontSize, ...props }: TextProps<"h2">) => {
-  return (
-    <h2 {...cnProps(props, fontSize ? styles[`text-fs-${fontSize}`] : "")}></h2>
-  );
-};
-
-Text.H3 = ({ fontSize, ...props }: TextProps<"h3">) => {
-  return (
-    <h3 {...cnProps(props, fontSize ? styles[`text-fs-${fontSize}`] : "")}></h3>
-  );
-};
-
-Text.H4 = ({ fontSize, ...props }: TextProps<"h4">) => {
-  return (
-    <h4 {...cnProps(props, fontSize ? styles[`text-fs-${fontSize}`] : "")}></h4>
-  );
-};
-
-Text.H5 = ({ fontSize, ...props }: TextProps<"h5">) => {
-  return (
-    <h5 {...cnProps(props, fontSize ? styles[`text-fs-${fontSize}`] : "")}></h5>
-  );
-};
-
-Text.H6 = ({ fontSize, ...props }: TextProps<"h6">) => {
-  return (
-    <h6 {...cnProps(props, fontSize ? styles[`text-fs-${fontSize}`] : "")}></h6>
-  );
-};
+export const Text = (props: TextProps<"p">) => <p {...handleProps(props)}></p>;
+Text.H1 = (props: TextProps<"h1">) => <h1 {...handleProps(props)}></h1>;
+Text.H2 = (props: TextProps<"h2">) => <h2 {...handleProps(props)}></h2>;
+Text.H3 = (props: TextProps<"h3">) => <h3 {...handleProps(props)}></h3>;
+Text.H4 = (props: TextProps<"h4">) => <h4 {...handleProps(props)}></h4>;
+Text.H5 = (props: TextProps<"h5">) => <h5 {...handleProps(props)}></h5>;
+Text.H6 = (props: TextProps<"h6">) => <h6 {...handleProps(props)}></h6>;
