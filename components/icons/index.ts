902d58f085cfb8b3fa6f54e65ae47aa3bad8ffe7
secona <77039267+secona@users.noreply.github.com>
2024-05-09 10:30:26 +0700

    feat: add torus

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as ElementA } from "./ElementA";
 export { default as ElementB } from "./ElementB";
 export { default as Logo } from "./Logo";
+export { default as Torus } from "./Torus";
